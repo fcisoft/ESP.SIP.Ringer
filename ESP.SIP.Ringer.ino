80da1dc9e7bf7ae066b2be30181d06f719261c8a
fcisoft <fcisoft@libero.it>
2022-05-10 12:03:32 +0200

    Update ESP.SIP.Ringer.ino

--- ESP.SIP.Ringer.ino
+++ ESP.SIP.Ringer.ino
@@ -1,3 +1,7 @@
+
+#include <ESP_WiFiManager.h>
+#include <ESP_WiFiManager.hpp>
+
 #include "config.h"
 #include <ESP8266WiFi.h>
 #include "ArduinoSIP.h"
@@ -33,17 +37,205 @@ void cancelCallback(const char * from){
   ring.stopRing();
 }
 
+#define FileFS        LITTLEFS
 
 void setup(){
   Serial.begin(115200);
   pinMode(ledPin,OUTPUT);
   digitalWrite(ledPin,HIGH);
+
+
+
+  FileFS.format();
+
+
+  Serial.println(F("SPIFFS/LittleFS failed! Already tried formatting."));
+  
+  if (!FileFS.begin()){     
+      // prevents debug info from the library to hide err message.
+      delay(100);
+      #if USE_LITTLEFS
+          Serial.println(F("LittleFS failed!. Please use SPIFFS or EEPROM. Stay forever"));
+      #else
+          Serial.println(F("SPIFFS failed!. Please use LittleFS or EEPROM. Stay forever"));
+      #endif
+      while (true){
+        delay(1);
+      }
+    }
+  }
+
+  drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
+
+  unsigned long startedAt = millis();
+
+  // New in v1.4.0
+  initAPIPConfigStruct(WM_AP_IPconfig);
+  initSTAIPConfigStruct(WM_STA_IPconfig);
+  //////
+
+
+  ESP_WiFiManager ESP_wifiManager("ConfigOnDoubleReset");
+
+ESP_wifiManager.setMinimumSignalQuality(-1);
+
+  // We can't use WiFi.SSID() in ESP32 as it's only valid after connected.
+  // SSID and Password stored in ESP32 wifi_ap_record_t and wifi_config_t are also cleared in reboot
+  // Have to create a new function to store in EEPROM/SPIFFS for this purpose
+  Router_SSID = ESP_wifiManager.WiFi_SSID();
+  Router_Pass = ESP_wifiManager.WiFi_Pass();
+
+  //Remove this line if you do not want to see WiFi password printed
+  Serial.println("ESP Self-Stored: SSID = " + Router_SSID + ", Pass = " + Router_Pass);
+// SSID/PWD to uppercase
+  ssid.toUpperCase();
+  password = "My" + ssid;
+
+  bool configDataLoaded = false;
+
+  // From v1.1.0, Don't permit NULL password
+  if ( (Router_SSID != "") && (Router_Pass != "") )
+  {
+    LOGERROR3(F("* Add SSID = "), Router_SSID, F(", PW = "), Router_Pass);
+    wifiMulti.addAP(Router_SSID.c_str(), Router_Pass.c_str());
+
+    ESP_wifiManager.setConfigPortalTimeout(120); //If no access point name has been previously entered disable timeout.
+    Serial.println(F("Got ESP Self-Stored Credentials. Timeout 120s for Config Portal"));
+  }
+  
+  if (loadConfigData())
+  {
+    configDataLoaded = true;
+    
+    ESP_wifiManager.setConfigPortalTimeout(120); //If no access point name has been previously entered disable timeout.
+    Serial.println(F("Got stored Credentials. Timeout 120s for Config Portal")); 
+
+  }
+  else
+  {
+    // Enter CP only if no stored SSID on flash and file
+    Serial.println(F("Open Config Portal without Timeout: No stored Credentials."));
+    initialConfig = true;
+  }
+
+  if (drd->detectDoubleReset())
+  {
+    // DRD, disable timeout.
+    ESP_wifiManager.setConfigPortalTimeout(0);
+
+    Serial.println(F("Open Config Portal without Timeout: Double Reset Detected"));
+    initialConfig = true;
+  }
+
+  if (initialConfig)
+  {
+    Serial.print(F("Starting configuration portal @ "));
+    
+    Serial.print(F(", SSID = "));
+    Serial.print(ssid);
+    Serial.print(F(", PWD = "));
+    Serial.println(password);
+
+    // Starts an access point
+    if (!ESP_wifiManager.startConfigPortal((const char *) ssid.c_str(), (const char *) password.c_str()))
+      Serial.println(F("Not connected to WiFi but continuing anyway."));
+    else
+    {
+      Serial.println(F("WiFi connected...yeey :)"));
+    }
+
+    // Stored  for later usage, from v1.1.0, but clear first
+    memset(&WM_config, 0, sizeof(WM_config));
+
+    for (uint8_t i = 0; i < NUM_WIFI_CREDENTIALS; i++)
+    {
+      String tempSSID = ESP_wifiManager.getSSID(i);
+      String tempPW   = ESP_wifiManager.getPW(i);
+
+      if (strlen(tempSSID.c_str()) < sizeof(WM_config.WiFi_Creds[i].wifi_ssid) - 1)
+        strcpy(WM_config.WiFi_Creds[i].wifi_ssid, tempSSID.c_str());
+      else
+        strncpy(WM_config.WiFi_Creds[i].wifi_ssid, tempSSID.c_str(), sizeof(WM_config.WiFi_Creds[i].wifi_ssid) - 1);
+
+      if (strlen(tempPW.c_str()) < sizeof(WM_config.WiFi_Creds[i].wifi_pw) - 1)
+        strcpy(WM_config.WiFi_Creds[i].wifi_pw, tempPW.c_str());
+      else
+        strncpy(WM_config.WiFi_Creds[i].wifi_pw, tempPW.c_str(), sizeof(WM_config.WiFi_Creds[i].wifi_pw) - 1);
+
+      // Don't permit NULL SSID and password len < MIN_AP_PASSWORD_SIZE (8)
+      if ( (String(WM_config.WiFi_Creds[i].wifi_ssid) != "") && (strlen(WM_config.WiFi_Creds[i].wifi_pw) >= MIN_AP_PASSWORD_SIZE) )
+      {
+        LOGERROR3(F("* Add SSID = "), WM_config.WiFi_Creds[i].wifi_ssid, F(", PW = "), WM_config.WiFi_Creds[i].wifi_pw );
+        wifiMulti.addAP(WM_config.WiFi_Creds[i].wifi_ssid, WM_config.WiFi_Creds[i].wifi_pw);
+      }
+    }
+
+
+
+    // New in v1.4.0
+    ESP_wifiManager.getSTAStaticIPConfig(WM_STA_IPconfig);
+    //////
+    
+    saveConfigData();
+  }
+
+
+
+ startedAt = millis();
+
+  if (!initialConfig)
+  {
+    // Load stored data, the addAP ready for MultiWiFi reconnection
+    if (!configDataLoaded)
+      loadConfigData();
+
+    for (uint8_t i = 0; i < NUM_WIFI_CREDENTIALS; i++)
+    {
+      // Don't permit NULL SSID and password len < MIN_AP_PASSWORD_SIZE (8)
+      if ( (String(WM_config.WiFi_Creds[i].wifi_ssid) != "") && (strlen(WM_config.WiFi_Creds[i].wifi_pw) >= MIN_AP_PASSWORD_SIZE) )
+      {
+        LOGERROR3(F("* Add SSID = "), WM_config.WiFi_Creds[i].wifi_ssid, F(", PW = "), WM_config.WiFi_Creds[i].wifi_pw );
+        wifiMulti.addAP(WM_config.WiFi_Creds[i].wifi_ssid, WM_config.WiFi_Creds[i].wifi_pw);
+      }
+    }
+
+    if ( WiFi.status() != WL_CONNECTED ) 
+    {
+      Serial.println(F("ConnectMultiWiFi in setup"));
+     
+      connectMultiWiFi();
+    }
+  }
+
+  Serial.print(F("After waiting "));
+  Serial.print((float) (millis() - startedAt) / 1000);
+  Serial.print(F(" secs more in setup(), connection result is "));
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.print(F("connected. Local IP: "));
+    Serial.println(WiFi.localIP());
+  }
+  else
+    Serial.println(ESP_wifiManager.getStatus(WiFi.status()));
+}
+
+
+
+    
+
+
+
+
+    
+/*  
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
+ */
   String ip = WiFi.localIP().toString();
   Serial.println("connesso, ip:"+ip);
   digitalWrite(ledPin,LOW);
@@ -56,6 +248,8 @@ void setup(){
 
 
 void loop(void){
+  drd->loop();
   aSip.loop(acSipIn, sizeof(acSipIn));
   ring.handle();
+  check_status();
 }
